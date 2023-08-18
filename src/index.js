7920cf8242e4a3a5467ee4b80021b5e7cc789c38
Obote <obotedenis256@gmail.com>
2023-08-18 19:34:22 +0300

    Add js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,50 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+document.addEventListener('DOMContentLoaded', () => {
+  const tasks = [
+    {description: 'Task 1', completed: false, index: 1},
+    {description: 'Task 2', completed: false, index: 2},
+    {description: 'Task 3', completed: false, index: 3},
+  ];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  function populateTaskList(tasks) {
+    const taskList = document.getElementById('tasks');
 
-  return element;
-}
+    tasks.sort((a,b) => a.index - b.index)
 
-document.body.appendChild(component());
+    taskList.forEach((task) => {
+      const listItem = document.createElement('li');
+      listItem.classList.add('list-item');
+
+      const listDiv = document.createElement('div');
+      listDiv.classList.add('list-div');
+
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.classList.add('check');
+      checkbox.checked = task.completed;
+      checkbox.addEventListener('change', function () {
+        if (this.checked) {
+          listItem.classList.add('check');
+        }else {
+          listItem.classList.remove('completed');
+        }
+      });
+
+      const description = document.createElement('p');
+      description.textContent = task.description;
+
+      const ellipsisIcon = document.createElement('i');
+      ellipsisIcon.classList.add('fa-solid', 'fa-ellipsis-vertical');
+
+      listDiv.appendChild(checkbox);
+      listDiv.appendChild(description);
+      listItem.appendChild(listDiv);
+      listItem.appendChild(ellipsisIcon);
+
+      taskList.appendChild(listItem);
+    });
+  }
+
+  populateTaskList(tasks);
+});
